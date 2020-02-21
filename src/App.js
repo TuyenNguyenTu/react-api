7e2f95c7ee56c1fdead8e349c06312356943e946
TuyenNguyen <tuyen.nguyen@amela.vn>
2020-02-21 22:02:13 +0700

    react-app: get data from web api with react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(params) {
+    super(params);
+    this.state = {
+      items:[],
+      isLoaded:false,
+
+    }
+  }
+  componentDidMount(){
+    //create actual API
+    //fetch("UrlAPI")
+    fetch('https://localhost:44325/api/Student')
+    .then(res=>res.json())
+    .then(json => {
+      this.setState({
+        isLoaded:true,
+        items: json,
+      })
+    })
+  }
+  render(){
+    var {isLoaded,items} = this.state;
+    if(!isLoaded){
+      return  <div>Loading...</div>;
+    }
+    else{
+      return (
+        <div className="App">
+          <table className="table table-hover">
+            <thead>
+              <tr>
+                <td>Name</td>
+                <td>Address</td>
+                <td>Age</td>
+                <td>Status</td>
+              </tr>
+            </thead>
+            <tbody>
+              {items.map(item=>(
+                <tr key={item.id}>
+                  <td>{item.name}</td>
+                  <td>{item.address}</td>
+                  <td>{item.age}</td>
+                  <td>{item.status===true?"Active":"Block"}</td>
+                </tr>
+              ))}
+              
+            </tbody>
+          </table>
+        </div>
+      );
+    }
+    
+  }
 }
 
 export default App;
